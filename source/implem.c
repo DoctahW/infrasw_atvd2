fc90a465f20fd5b58b208347282fa6db1b2ece24
Euclides <joaomartires2@gmail.com>
2026-08-30 19:41:42 -0300

    fix: parar de ter unused parameter

--- source/implem.c
+++ source/implem.c
@@ -2,7 +2,7 @@
 
 void mandel_serial(unsigned char *pixels, int largura, int altura, 
     int max_iteracoes, int n_threads) {
-
+    (void)n_threads;
     for (int y = 0; y < altura; y++) {
         for (int x = 0; x < largura; x++) {
             mandel_pixel(pixels, x, y, largura, altura, max_iteracoes);
